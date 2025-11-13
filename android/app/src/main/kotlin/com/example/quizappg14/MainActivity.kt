887838490e0cc0684c4ab7f0ee2c2bf22ed7c9d9
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-11-12 19:13:11 -0500

    first

---
+++ android/app/src/main/kotlin/com/example/quizappg14/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.quizappg14
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:quizappg14/pages/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
+}
